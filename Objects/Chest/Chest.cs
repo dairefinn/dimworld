8ec45e3162c3ae60c4cf5c9dfbd0cb7066788d51
Daire Finn <dmanfinn@gmail.com>
2025-03-19 20:27:16 +0000

    Added sword object and commented plan for Chest entity

--- Objects/Chest/Chest.cs
+++ Objects/Chest/Chest.cs
@@ -5,11 +5,12 @@ using Godot;
 public partial class Chest : StaticBody2D, ICanBeInteractedWith
 {
 
+    // TODO: This currently just adds a sword to your inventory but it should open a screen where you can compare inventories
     public void InteractWith()
     {
         GD.Print("Interacting with chest");
 
-        InventoryItem itemDuplicate = GD.Load("res://Items/TestItem.tres").Duplicate() as InventoryItem;
+        InventoryItem itemDuplicate = GD.Load("res://Items/Sword.tres").Duplicate() as InventoryItem;
         bool success = Globals.GetInstance().MainPlayer.Inventory.AddItem(itemDuplicate);
         if (success)
         {
