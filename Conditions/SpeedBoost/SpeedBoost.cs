0cf8fb148f1218cc7c0d0881800168041b101626
Daire Finn <dmanfinn@gmail.com>
2025-03-31 13:08:10 +0100

    Speed boost changes character speed instead of velocity directly

--- Conditions/SpeedBoost/SpeedBoost.cs
+++ Conditions/SpeedBoost/SpeedBoost.cs
@@ -5,17 +5,43 @@ using Godot;
 
 public partial class SpeedBoost : Condition
 {
-    public override void OnPhysicsProcess(double delta, IAffectedByConditions target)
-    {
-        if (target is CharacterBody3D targetCharacterBody3D)
-        {
-            targetCharacterBody3D.Velocity *= 1.5f;
-        }
-        else if (target is CharacterBody2D targetCharacterBody2D)
-        {
-            targetCharacterBody2D.Velocity *= 1.5f;
-        }
 
-        base.OnProcess(delta, target);
+    public override void ApplyTo(IAffectedByConditions target)
+    {
+        base.ApplyTo(target);
+        
+        if (target is CharacterController characterController)
+        {
+            characterController.Speed *= 2;
+        }
     }
+
+    public override void RemoveFrom(IAffectedByConditions target)
+    {
+        base.RemoveFrom(target);
+
+        if (target is CharacterController characterController)
+        {
+            characterController.Speed /= 2;
+        }
+    }
+
+    // public override void OnPhysicsProcess(double delta, IAffectedByConditions target)
+    // {
+    //     if (target is CharacterController characterController)
+    //     {
+    //         characterController.Speed *= 1.5f;
+    //     }
+    //     else if (target is CharacterBody3D targetCharacterBody3D)
+    //     {
+    //         targetCharacterBody3D.Velocity *= 1.5f;
+    //     }
+    //     else if (target is CharacterBody2D targetCharacterBody2D)
+    //     {
+    //         targetCharacterBody2D.Velocity *= 1.5f;
+    //     }
+
+    //     base.OnProcess(delta, target);
+    // }
+
 }
