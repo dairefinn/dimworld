25b2e94782987b7d1db18f41d01b59bac5b00476
Daire Finn <dmanfinn@gmail.com>
2025-04-01 14:34:39 +0100

    Speech bubble that works in engine and editor

--- Agents/Dialogue/SpeechBubble.cs
+++ Agents/Dialogue/SpeechBubble.cs
@@ -1,7 +1,6 @@
 namespace Dimworld;
 
 using Godot;
-using System;
 
 
 [Tool] // Enables the script to run in the editor
@@ -10,31 +9,65 @@ public partial class SpeechBubble : Control
     [Export] public string Text
     {
         get => _text;
-        set => SetText(value);
+        set {
+            _text = value;
+            OnTextUpdated(value);
+        }
     }
     private string _text;
 
     [Export] public Label Label { get; set; }
 
+    /// <summary>
+    /// The timer that controls how long the speech bubble is visible. If left empty, the speech bubble will be visible indefinitely as long as the text is not empty.
+    /// </summary>
+    [Export] public Timer HideTimer { get; set; }
 
-    private void SetText(string text)
+
+    public override void _Ready()
     {
-        GD.Print($"Setting text to: {text}");
-        _text = text;
+        base._Ready();
 
+        if (HideTimer == null)
+        {
+            HideTimer = GetNode<Timer>("HideTimer");
+        }
+        
+        if (HideTimer != null)
+        {
+            HideTimer.Timeout += OnTimerTimeout;
+        }
+    }
+
+
+    private void OnTextUpdated(string text)
+    {
         if (Label != null)
         {
-            Label.Text = _text;
+            Label.Text = text;
 
-            if (_text == null || _text.Length > 0)
+            if (text == null || text.Length > 0)
             {
                 Show();
+                StartHideTimer();
             }
             else
             {
                 Hide();
             }
         }
+    }
+
+    private void StartHideTimer()
+    {
+        if (!IsInstanceValid(HideTimer) || !HideTimer.IsInsideTree()) return; // Ensure Timer is valid and in the scene tree
+        HideTimer?.Start();
+    }
+
+    private void OnTimerTimeout()
+    {
+        if (Engine.IsEditorHint()) return; // Don't hide the speech bubble in the editor
+        Hide();
     }
 
 }
