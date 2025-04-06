0b18d89de05f050f141764418b55edae736fe9bd
Daire Finn <dmanfinn@gmail.com>
2025-04-06 01:20:58 +0100

    Sword does damage in the direction of the mouse cursor

--- Objects/Bomb/Bomb.cs
+++ Objects/Bomb/Bomb.cs
@@ -7,12 +7,6 @@ using Godot;
 public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 {
 
-    public static readonly PackedScene EFFECT_EXPLOSION = GD.Load<PackedScene>("res://Effects/DamageInstant/DamageInstant.tscn");
-    public static readonly PackedScene EFFECT_DAMAGE_OVER_TIME = GD.Load<PackedScene>("res://Effects/DamageOverTime/DamageOverTime.tscn");
-    public static readonly PackedScene EFFECT_PUSH_PULL = GD.Load<PackedScene>("res://Effects/PushPull/PushPull.tscn");
-    public static readonly PackedScene EFFECT_APPLY_MODIFIER = GD.Load<PackedScene>("res://Effects/ApplyModifier/ApplyModifier.tscn");
-
-
     public void InteractWith()
     {
         // TriggerExplosion();
@@ -24,7 +18,7 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 
     private void TriggerExplosion()
     {
-        DamageInstant explosion = EFFECT_EXPLOSION.Instantiate<DamageInstant>();
+        DamageInstant explosion = Effects.DAMAGE_INSTANT.Instantiate<DamageInstant>();
         explosion.Damage = 100;
         explosion.Radius = 200;
         explosion.Duration = 1;
@@ -33,7 +27,7 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 
     private void TriggerFire()
     {
-        DamageOverTime fire = EFFECT_DAMAGE_OVER_TIME.Instantiate<DamageOverTime>();
+        DamageOverTime fire = Effects.DAMAGE_OVER_TIME.Instantiate<DamageOverTime>();
         fire.Damage = 10;
         fire.Radius = 100;
         fire.Duration = 60;
@@ -43,7 +37,7 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 
     private void TriggerKnockback()
     {
-        PushPull knockback = EFFECT_PUSH_PULL.Instantiate<PushPull>();
+        PushPull knockback = Effects.PUSH_PULL.Instantiate<PushPull>();
         knockback.Radius = 100;
         knockback.Force = 1000;
         knockback.Direction = PushPull.DirectionType.PUSH;
@@ -52,7 +46,7 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 
     private void TriggerPull()
     {
-        PushPull pull = EFFECT_PUSH_PULL.Instantiate<PushPull>();
+        PushPull pull = Effects.PUSH_PULL.Instantiate<PushPull>();
         pull.Radius = 100;
         pull.Force = 1000;
         pull.Direction = PushPull.DirectionType.PULL;
@@ -63,12 +57,12 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
     {
         float duration = 0.1f;
 
-        DamageInstant explosion = EFFECT_EXPLOSION.Instantiate<DamageInstant>();
+        DamageInstant explosion = Effects.DAMAGE_INSTANT.Instantiate<DamageInstant>();
         explosion.Damage = 10;
         explosion.Radius = 200;
         explosion.Duration = duration;
 
-        PushPull knockback = EFFECT_PUSH_PULL.Instantiate<PushPull>();
+        PushPull knockback = Effects.PUSH_PULL.Instantiate<PushPull>();
         knockback.Radius = 100;
         knockback.Force = 3000;
         knockback.Direction = PushPull.DirectionType.PUSH;
@@ -79,7 +73,7 @@ public partial class Bomb : StaticBody2D, ICanBeInteractedWith
 
     private void TriggerApplyModifier()
     {
-        ApplyModifier applyModifier = EFFECT_APPLY_MODIFIER.Instantiate<ApplyModifier>();
+        ApplyModifier applyModifier = Effects.APPLY_MODIFIER.Instantiate<ApplyModifier>();
         // applyModifier.Modifier = new VelocityMultiplyModifier("bomb_speed_boost", 2f).SetDuration(5);
         // applyModifier.Modifier = new VelocityMultiplyModifier("bomb_slow_down", 0.5f).SetDuration(5);
         applyModifier.Modifier = new HealthMultiplyModifier("bomb_health_boost", 2f).SetHealOnAdd(true).SetDuration(5);
